2e4b875cf416be329dfb9b415316f3d1cb5be25b
donny_444 <104888835+donny444@users.noreply.github.com>
2023-06-27 21:54:17 +0700

    Updated!

--- append_date.js
+++ append_date.js
@@ -1,4 +1,3 @@
-const http = require("http");
 const fs = require("fs");
 
 var date = new Date();
@@ -6,7 +5,7 @@ var year = date.getFullYear().toString();
 var month = date.getMonth().toString();
 var day = date.getDate().toString();
 
-var data = "Checked on " + day + "/" + month + "/" + year;
+var data = "Checked on " + day + "/" + month + "/" + year + "\n";
 
 fs.appendFile('check_on_run.txt', data, function(err) {
     if (err) throw err;
