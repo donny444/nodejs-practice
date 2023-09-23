27fca5430eb04863f58d50cdf6fa045d77476fdb
donny_444 <104888835+donny444@users.noreply.github.com>
2023-09-23 23:17:35 +0700

    Add files via upload

--- govinfo-api/app.js
+++ govinfo-api/app.js
@@ -2,19 +2,52 @@ require("dotenv").config();
 const axios = require("axios");
 const express = require("express");
 const flatted = require("flatted");
+const util = require("util");
+const { stringify } = require("./utils.js");
 const app = express();
 app.use(express.json());
 
 app.get("/collections", async (req, res) => {
     const options = {
         method: "GET",
-        url: `https://api.govinfo.gov/collections?api_key=${process.env.API_KEY}`
+        url: `https://api.govinfo.gov/collections`,
+        params: {
+            api_key: process.env.API_KEY
+        },
+        headers: {
+            "Accept": "application/json"
+        }
     }
 
     try {
         const response = await axios.request(options);
-        return res.status(200).send(flatted.stringify(response));
-    } catch(err) {
+
+        //return res.status(200).send(util.inspect(response["data"]["collections"]));
+        //return res.status(200).send(flatted.stringify(response["data"]["collections"]));
+        return res.status(200).send(stringify(response["data"]["collections"]));
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+app.get("/collections/:collection/:lastModifiedStartDate", async (req, res) => {
+    const options = {
+        method: "GET",
+        url: `https://api.govinfo.gov/collections`,
+        params: {
+            offset: req.query.offset,
+            pageSize: req.query.pageSize,
+            api_key: process.env.API_KEY
+        },
+        headers: {
+            "Accept": "application/json"
+        }
+    }
+
+    try {
+        const response = await axios.request(options);
+        return res.status(200).send(stringify(response["data"]["collections"]));
+    } catch (err) {
         console.error(err);
     }
 })
