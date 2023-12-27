305fb2a0e5192ce075111ab61c7e4d4567638ac7
donny_444 <104888835+donny444@users.noreply.github.com>
2023-12-27 16:38:49 +0700

    Delete openweather-api directory

--- openweather-api/server.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const http = require("http");
-const app = require("./app.js");
-const server = http.createServer(app);
-
-const { PORT } = process.env;
-const port = process.env.PORT || PORT
-
-server.listen(port, () => console.log(`Server is running on port ${port}`));
